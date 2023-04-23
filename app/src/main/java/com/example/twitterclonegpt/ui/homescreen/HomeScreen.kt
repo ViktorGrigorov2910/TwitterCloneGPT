9e664344f9926a5678a9857bb6bc968d00e9d54d
Viktor <viktor_grigorov@abv.bg>
2023-04-23 22:04:45 +0300

    Refactored some code, create separate file for actions on the post

--- app/src/main/java/com/example/twitterclonegpt/ui/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/twitterclonegpt/ui/homescreen/HomeScreen.kt
@@ -3,20 +3,11 @@ package com.example.twitterclonegpt.ui.homescreen
 import android.annotation.SuppressLint
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import dagger.hilt.android.AndroidEntryPoint
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomeScreen() {
     val viewModel: HomeScreenViewModel = hiltViewModel()
     Scaffold { HomeScreenContent(viewModel) }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomeScreenPreview() {
-//    HomeScreen()
 }
---
+++ app/src/main/java/com/example/twitterclonegpt/ui/homescreen/post/PostActionStrip.kt
@@ -0,0 +1,2 @@
+package com.example.twitterclonegpt.ui.homescreen.post
+
